5a78316c36f68af2d49f0705eaebeb794cb5225c
Andy Grossberg <andy.grossberg@gmail.com>
2018-04-20 14:47:46 -0700

    save to migrate database

--- Gummi/Models/Person.cs
+++ Gummi/Models/Person.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Gummi.Models
-{
-    [Table("People")]
-    public class Person
-    {
-        [Key]
-        public int PersonId { get; set; }
-        public string Name { get; set; }
-        public int ExperienceId { get; set; }
-        //public ICollection<Experience> Experiences { get; set; }
-        //public ICollection<Location> Locations { get; set; }
-    }
-}
+//namespace Gummi.Models
+//{
+//    [Table("People")]
+//    public class Person
+//    {
+//        [Key]
+//        public int PersonId { get; set; }
+//        public string Name { get; set; }
+//        public int ExperienceId { get; set; }
+//        //public ICollection<Experience> Experiences { get; set; }
+//        //public ICollection<Location> Locations { get; set; }
+//    }
+//}
