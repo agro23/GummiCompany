9269340518bf9edf97e2b79d8dbce4e2ae53b39e
Andy Grossberg <andy.grossberg@gmail.com>
2018-04-27 13:28:09 -0700

    Add Review model with CRUD and alter Product properties

---
+++ Gummi/Controllers/ReviewController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Gummi.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Gummi.Controllers
+{
+    public class ReviewssController : Controller
+    {
+        private GummiDbContext db = new GummiDbContext();
+        public IActionResult Index()
+        {
+            List<Review> model = db.Reviews.ToList();
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Review review)
+        {
+            db.Reviews.Add(review);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var thisReview = db.Reviews.FirstOrDefault(Reviews => Reviews.ReviewId == id);
+            return View(thisReview);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Review review)
+        {
+            db.Entry(review).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisReview = db.Reviews.FirstOrDefault(Reviews => Reviews.ReviewId == id);
+            return View(thisReview);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var thisReview = db.Reviews.FirstOrDefault(Reviews => Reviews.ReviewId == id);
+            return View(thisReview);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisReview = db.Reviews.FirstOrDefault(Reviews => Reviews.ReviewId == id);
+            db.Reviews.Remove(thisReview);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
